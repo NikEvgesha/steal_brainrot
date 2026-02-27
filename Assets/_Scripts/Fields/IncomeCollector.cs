158ec3d32e8a4c4882fb6e100fd88693d2946150
NikEvgesha <nikitenkowa.e@yandex.ru>
2026-02-27 13:58:35 +0300

    добавлен сбор дохода на сцену

---
+++ Assets/_Scripts/Fields/IncomeCollector.cs
@@ -0,0 +1,69 @@
+using System;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class IncomeCollector : MonoBehaviour
+{
+    [SerializeField] private int _gemPrice;
+    [SerializeField] private Text _priceText;
+
+    [SerializeField] private GameObject _ui;
+    private bool _collectorPurchased;
+    private bool _purchaseInProgress;
+
+    [HideInInspector] public UnityEvent CollectIncome;
+
+    private void Start()
+    {
+        _priceText.text = _gemPrice.ToString();
+    }
+
+    public void _onPlayerEnter() {
+        if (_collectorPurchased)
+        {
+            CollectIncome?.Invoke();
+        }
+        else
+        {
+            Open();
+        }
+    }
+
+    public void _onPlayerExit() {
+        if (!_collectorPurchased)
+            Close();
+    }
+
+    public void _TryBuy() {
+        if (G.Currency.RemoveCurrency(CurrencyType.Gems, _gemPrice)) {
+            _collectorPurchased = true;
+            Destroy(_ui);
+        }
+    }
+
+    public void _ShowAd() {
+        if (G.Ad == null) return;
+
+        _purchaseInProgress = true;
+        G.Ad.ShowRewardedAd("IncomeCollect", success =>
+        {
+            _purchaseInProgress = false;
+            if (!success) return;
+            CollectIncome?.Invoke();
+        });
+        return;
+    }
+
+
+    private void Open()
+    {
+        _ui.gameObject.SetActive(true);
+    }
+
+
+    private void Close()
+    {
+        _ui.gameObject.SetActive(false);
+    }
+}
